ad06ab18081d4834ddc615c3f9b8c2deef003c22
Karol Gurba <karolgurba22@gmail.com>
2020-10-15 18:30:07 -0500

    added url route to users profile page

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register' ),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login' ),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' )
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout' ),
+    path('profile/', auth_views.LogoutView.as_view(template_name='users/profile.html'), name='profile' )
 ]
