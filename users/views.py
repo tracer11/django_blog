65ecf1bf5538da3670a789d83130035a11480e17
Karol Gurba <karolgurba22@gmail.com>
2020-10-20 14:02:31 -0500

    added default image and a profile will now be created with every new user

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
-      messages.success(request, f'Welcome to the blog f{username}!')
+      messages.success(request, f'Welcome to the blog {username}!')
       return redirect('login')
   else:
     form = UserRegisterForm()
