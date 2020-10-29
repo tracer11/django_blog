b39fc858dcea480bf384806aafa101f3fbf3dd92
Karol Gurba <karolgurba22@gmail.com>
2020-10-29 15:28:46 -0500

    create post logic

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostCreation
 
 def home(request):
   context = {
@@ -8,6 +9,19 @@ def home(request):
   return render(request, 'blog/home.html', context)
 
 
+def create(request):
+  if request.method == 'POST':
+    form = PostCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      redirect('')
+
+  context = {'form' : form}
+
+  return render(request, 'blog/create_post.html', context)
+
+
 def about(request):
   return render(request, 'blog/about.html')
+
 
