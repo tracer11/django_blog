780f03f118c7643394b34c792a504bd3d9ebaef5
Karol Gurba <karolgurba22@gmail.com>
2020-10-29 15:35:00 -0500

    more post creation logic

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('create/', views.create, name='blog-make-post'),
 ]
 
 
