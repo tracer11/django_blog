b34f71fb1760a2574e43c5e09e39f3ecd4b40353
Karol Gurba <karolgurba22@gmail.com>
2020-10-28 20:52:33 -0500

    added django-cleanup package to auto delete old profile pics

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,15 +28,16 @@ ALLOWED_HOSTS = []
 # Application definition  
 
 INSTALLED_APPS = [ 
-    'users.apps.UsersConfig',
+    'users.apps.UsersConfig', 
+    'blog.apps.BlogConfig',
     'crispy_forms', 
-    'blog.apps.BlogConfig', 
     'django.contrib.admin', 
     'django.contrib.auth',  
     'django.contrib.contenttypes',  
     'django.contrib.sessions',  
     'django.contrib.messages',  
     'django.contrib.staticfiles', 
+    'django_cleanup'
 ] 
 
 MIDDLEWARE = [  
