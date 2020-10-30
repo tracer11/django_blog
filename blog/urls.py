a5eb6239fc62e5c3958ff362cf879435cae0690e
Karol Gurba <karolgurba22@gmail.com>
2020-10-30 17:02:11 -0500

    added update post logic

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views 
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='blog-detail'),
     path('create/', PostCreateView.as_view(), name='blog-create'),
+    path('update/<int:pk>', PostUpdateView.as_view(), name='blog-update'),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView)
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 class PostListView(ListView):
@@ -12,7 +13,7 @@ class PostDetailView(DetailView):
   model = Post
   template_name = 'blog/detail.html'
 
-class PostCreateView(CreateView): 
+class PostCreateView(LoginRequiredMixin,  CreateView): 
     model = Post
     fields = ['title', 'content']
 
@@ -20,7 +21,13 @@ class PostCreateView(CreateView):
       form.instance.author = self.request.user
       return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
 
+    def form_valid(self, form):
+      form.instance.author = self.request.user
+      return super().form_valid(form)
 
 
 
